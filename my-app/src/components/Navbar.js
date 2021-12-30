ff5b60e26887dfea5d3b9c7dd859e81adf862777
ivanbalderrama <b.ivan0907@gmail.com>
2021-12-29 21:13:06 -0600

    added files,components, and styled navbar

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../images/logo.svg';
+import "../style.css";
+
+function Navbar() {
+    return (
+        <nav className="navBar">
+        <img src={Logo} className="reactLogo"></img>
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+export default Navbar;
